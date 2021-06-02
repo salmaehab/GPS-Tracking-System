f4408b3309976b3d8620dc0255dfaeb0cbe890b9
omarelshamy12 <72103983+omarelshamy12@users.noreply.github.com>
2021-06-02 20:08:24 +0200

    write systick timer func

--- GPS Tracking System/GPS Tracking System.c
+++ GPS Tracking System/GPS Tracking System.c
@@ -70,6 +70,28 @@ void init(void){
 
 
 //systick timer
+//systick timer
+
+void SysTick_Wait(uint32_t delay)
+{ 
+    NVIC_ST_CTRL_R = 0;            /* (1) disable SysTick during setup */
+    NVIC_ST_RELOAD_R = delay-1;    /* (2) number of counts to wait */
+    NVIC_ST_CURRENT_R = 0;         /* (3) any value written to CURRENT clears */
+    NVIC_ST_CTRL_R |= 0x5;         /* (4) enable SysTick with core clock */
+ 
+    while((NVIC_ST_CTRL_R&0x00010000)==0) {
+        ;                          /* wait for COUNT flag */
+    }
+}
+
+void Delay(uint32_t delay){
+unsigned long i; 
+for(i=0; i<delay; i++){
+SysTick_Wait(800000); // wait 10ms
+}
+}
+
+
 
 
 
