f778787d4a8add6a39c16b45d40099d231abd59a
salmaehab <67350115+salmaehab@users.noreply.github.com>
2021-06-02 20:01:05 +0200

    port F and Port D init

--- GPS Tracking System/GPS Tracking System.c
+++ GPS Tracking System/GPS Tracking System.c
@@ -3,7 +3,35 @@
 
 // Port init
 
+void SystemInit() {}
+	uint32_t delay;
+void init(void){
+	//PORT F 
+	SYSCTL_RCGCGPIO_R |= 0x20;
+	delay =1 ;
+	GPIO_PORTF_LOCK_R = 0x4C4F434B;
+	GPIO_PORTF_CR_R = 0x1F;
+	GPIO_PORTF_AFSEL_R = 0;
+	GPIO_PORTF_PCTL_R = 0;
+	GPIO_PORTF_AMSEL_R = 0;
+	GPIO_PORTF_DIR_R = 0x0E;
+	GPIO_PORTF_DEN_R = 0x1F;
+	GPIO_PORTF_PUR_R = 0x11;
 
+
+	//port D
+
+	SYSCTL_RCGCGPIO_R |= 0x08;
+	delay = 1;
+
+	GPIO_PORTD_LOCK_R = 0x4C4F434B;
+	GPIO_PORTD_CR_R = 0xFF;
+	GPIO_PORTD_AFSEL_R = 0xF0;
+	GPIO_PORTD_PCTL_R = 0x11110000;
+	GPIO_PORTD_AMSEL_R = 0;
+	GPIO_PORTD_DIR_R = 0xA0;
+	GPIO_PORTD_DEN_R = 0xFF;
+}
 
 
 
